65e3b8e4c0ea4bec1b4298263ce39dcc690d49db
NinaEncinosa <67855315+NinaEncinosa@users.noreply.github.com>
2023-07-31 20:32:07 -0300

    V6 - Add routes, guards & views

--- LAMANSYS/FE/todo-list/src/app/services/auth.service.ts
+++ LAMANSYS/FE/todo-list/src/app/services/auth.service.ts
@@ -7,14 +7,14 @@ export class AuthService{
   isLoggedIn !: boolean;
 
   constructor() { 
-    this.isLoggedIn = false;
+    this.isLoggedIn = this.isUserLoggedIn();
   }
 
   login(username: string, password: string): boolean {
     //usuario y contraseña dados en manos a la obra VI
     if (username == 'admin' && password == '1234') {
-      localStorage.setItem('username', username);
-      localStorage.setItem('password', password);
+      sessionStorage.setItem('username', username);
+      sessionStorage.setItem('password', password);
       this.isLoggedIn = true;
       return true;
     }
@@ -23,8 +23,20 @@ export class AuthService{
   }
 
   logout(): any {
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('password');
+    this.isLoggedIn = false;
+  }
+
+  isUserLoggedIn(): boolean {
+    let user = sessionStorage.getItem('username');
+    let pass = sessionStorage.getItem('password');
+    if (user == 'admin' && pass == '1234') {
+      this.isLoggedIn = true;
+      return true;
+    }
+    this.isLoggedIn = false;
+    return false;
   }
 
 }
